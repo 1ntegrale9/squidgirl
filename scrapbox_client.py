3da951671128181ffb036ffd16f4df86958d345c
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2018-12-22 17:46:19 +0900

    bugfix Scrapboxの行リスト取得方法の変更

--- scrapbox_client.py
+++ scrapbox_client.py
@@ -6,4 +6,4 @@ URL_API = 'https://scrapbox.io/api/pages'
 def getDescriptions(projectName, pageTitle):
     URL = f'{URL_API}/{projectName}/{pageTitle}/text'
     res = requests.get(URL)
-    return res.text.split()[1:]
+    return res.text.split('\n')[1:]
