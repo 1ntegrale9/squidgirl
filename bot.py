75b382cd514bc3aaea2ac3372d70e923f3ffbde4
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2018-12-24 21:54:35 +0900

    Fix: 文字列型を排除、セット型で統一

--- bot.py
+++ bot.py
@@ -31,14 +31,20 @@ async def on_message(message):
                 if len(args) == 3 and str(client.user.id) in args[0] and args[1] == '教えて':
                     key = f'{message.guild.id}:{args[2]}'
                     if r.exists(key):
-                        return f'{args[2]} は {r.get(key).decode()} だよ！'
+                        msg = f'{args[2]} は {list(r.smembers(key))[0].decode()} だよ！'
                     else:
-                        return 'それは知らないよ！'
+                        msg = 'それは知らないよ！'
                 elif len(args) == 4 and str(client.user.id) in args[0] and args[1] == '覚えて':
                     key = f'{message.guild.id}:{args[2]}'
-                    r.set(key, args[3])
-                    r.sadd(str(message.guild.id), args[2])
-                    return f'{args[2]} は {args[3]}、覚えた！'
+                    if r.exists(key):
+                        old = list(r.smembers(key))[0].decode()
+                        r.sadd(key, args[3])
+                        r.sadd(str(message.guild.id), args[2])
+                        msg = f'{args[2]} は {old} じゃなかったっけ？\n{args[3]} で覚え直したよ！'
+                    else:
+                        r.sadd(key, args[3])
+                        r.sadd(str(message.guild.id), args[2])
+                        msg = f'{args[2]} は {args[3]}、覚えた！'
                 elif anyIn(message.content, ['ふとん', '布団']):
                     msg = await sleep(client, message)
                 elif anyIn(message.content, ['黒歴史']):
