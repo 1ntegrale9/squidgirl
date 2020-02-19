c9e847c696dee9fd9a944ee286931de804ab99ee
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-02-19 21:39:51 +0900

    :sparkles: 参加人数お知らせ機能

---
+++ src/fedibird/count21.py
@@ -0,0 +1,17 @@
+import os
+from mastodon import Mastodon
+
+fedibird = Mastodon(
+    access_token=os.getenv('MASTODON_TOKEN'),
+    api_base_url='https://fedibird.com',
+)
+
+statuses = sorted(
+    fedibird.account_statuses(77150),
+    key=lambda status: status['created_at'],
+    reverse=True,
+)
+status_poll = [status['poll'] for status in statuses if status['poll'] is not None][0]
+votes = [poll['votes_count'] for poll in status_poll['options'] if poll['title'] == '21時から参加可能'][0]
+
+fedibird.toot(f'21時から{votes}人が参加できるらしいよ！ #今日のプラベ')
