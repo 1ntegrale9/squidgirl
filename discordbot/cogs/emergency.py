6171de953635891626ffd3d9dee09cca98784ef8
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-05-15 00:57:41 +0900

    Feat: りんじの再生成コマンドを追加

--- discordbot/cogs/emergency.py
+++ discordbot/cogs/emergency.py
@@ -6,6 +6,26 @@ class Emergency(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def optimize(self, ctx):
+        if ctx.guild.id != self.bot.id_guild_ikatodon:
+            return
+        emergency = ctx.guild.get_channel(self.bot.id_category_emergency)
+        for ch in emergency.voice_channels:
+            if len(ch.members) > 0:
+                return await ctx.send('まだ人がいるよ！')
+        for ch in emergency.channels:
+            await ch.delete()
+        for i in range(1, 6, 1):
+            await ctx.guild.create_text_channel(
+                name=f'りんじ{i}',
+                category=emergency
+            )
+            await ctx.guild.create_voice_channel(
+                name=f'りんじ{i}',
+                category=emergency
+            )
+
     async def rename(self, message):
         vc = message.author.voice.channel
         if not vc:
